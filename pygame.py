48b3da9ff08c14289afa56af50a65eb3c424cb6a
Mackenzie Francisco <mackfran@umich.edu>
2016-11-25 15:12:11 -0500

    the basics

--- pygame.py
+++ pygame.py
@@ -2,3 +2,20 @@
 # uniqname: mackfran
 # Section Day/Time: Thursday/1-2PM
 # References:
+
+import pygame
+pygame.init();
+
+# creates colors
+white = (255,255,255)
+black = (0,0,0)
+red = (255, 0, 0)
+green = (0, 255, 0)
+
+# creates a surface
+gameDisplay = pygame.display.set_mode((800,600)) # initializes with a tuple
+
+# adds a title
+pygame.display.set_caption("Decorating Disaster!")
+
+pygame.display.update()
