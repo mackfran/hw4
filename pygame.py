2fb2f2feac462d371835ef4f1f1ba6555eafacd8
Mackenzie Francisco <mackfran@umich.edu>
2016-11-26 19:43:47 -0500

    color update

--- pygame.py
+++ pygame.py
@@ -11,6 +11,7 @@ white = (255,255,255)
 black = (0,0,0)
 red = (255, 0, 0)
 green = (0, 255, 0)
+blue = (0, 0, 255)
 
 # creates a surface
 gameDisplay = pygame.display.set_mode((800,600)) # initializes with a tuple
