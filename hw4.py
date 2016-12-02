035f51ef210fc5fc78b75fb6cb0e54265c89e7ef
Mackenzie Francisco <mackfran@umich.edu>
2016-12-02 14:25:38 -0500

    multiple coffees

--- hw4.py
+++ hw4.py
@@ -3,13 +3,14 @@
 # Section Day/Time: Thursday/1-2PM
 # References:
 	## http://programarcadegames.com/index.php?chapter=bitmapped_graphics_and_sound
-	## 
+	## http://programarcadegames.com/python_examples/f.php?file=timer.py
+	## http://stackoverflow.com/questions/23982907/python-library-pygame-centering-text
 
 from pygame import *
 from pygame.sprite import *
 from random import *
 
-DELAY = 1000;
+DELAY = 800;
 bgcolor = (0,42,196)
 
 class Coffee(Sprite):
@@ -22,6 +23,17 @@ class Coffee(Sprite):
 		randX = randint(0, 750)
 		randY = randint(0, 550)
 		self.rect.center = (randX,randY)
+
+class Kitten(Sprite):
+	def __init__(self):
+		Sprite.__init__(self)
+		self.image = image.load("kitten.bmp").convert_alpha()
+		self.rect = self.image.get_rect()
+
+	def move(self):
+		randX = randint(0, 750)
+		randY = randint(0, 550)
+		self.rect.center = (randX, randY)
 
 class Mug(Sprite):
 	def __init__(self):
@@ -39,11 +51,11 @@ init()
 
 # creates a surface
 screen = pygame.display.set_mode((800,600)) # initializes with a tuple
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
 
 # adds a title
 pygame.display.set_caption("Gilmore Girls")
-
-clock = pygame.time.Clock()
 
 # sets image positions
 background_position = [0,0]
@@ -51,14 +63,32 @@ background_position = [0,0]
 # loads images
 background_image = pygame.image.load("gilmore-girls.bmp").convert()
 
-f = font.Font("RobotoSlab-Regular.ttf", 25)
+f1 = font.Font("RobotoSlab-Regular.ttf", 25)
+f2 = font.Font("RobotoSlab-Regular.ttf", 120)
 
-coffee = Coffee()
+coffee1 = Coffee()
+coffee2 = Coffee()
+coffee3 = Coffee()
+coffee4 = Coffee()
+coffee5 = Coffee()
+kitten = Kitten()
 mug = Mug()
-sprites = RenderPlain(coffee, mug)
+
+coffee_list = pygame.sprite.Group()
+# for i in range(5):
+# 	cof = Coffee(coffee_list)
+# 	cof.add(coffee_list)
+
+sprites = RenderPlain(kitten, mug)
+coffee_sprites = RenderPlain(coffee1, coffee2, coffee3, coffee4, coffee5)
 
 hits = 0
 time.set_timer(USEREVENT + 1, DELAY)
+
+clock = pygame.time.Clock()
+frame_count = 0
+frame_rate = 25
+start_time = 30
 
 pygame.mixer.music.load("gilmoresong.wav")
 pygame.mixer.music.play(loops=-1)
@@ -70,23 +100,65 @@ while not gameExit:
 	for event in pygame.event.get():
 		if event.type == QUIT:
 			gameExit = True
-		elif event.type == MOUSEBUTTONDOWN:
-			if mug.hit(coffee):
-				coffee.move()
+		elif event.type == MOUSEMOTION:
+			if mug.hit(coffee1):
+				coffee1.move()
 				hits += 1
 				time.set_timer(USEREVENT + 1, DELAY)
+			if mug.hit(coffee2):
+				coffee2.move()
+				hits += 1
+				time.set_timer(USEREVENT + 1, DELAY)
+			if mug.hit(coffee3):
+				coffee3.move()
+				hits += 1
+				time.set_timer(USEREVENT + 1, DELAY)
+			if mug.hit(coffee4):
+				coffee4.move()
+				hits += 1
+				time.set_timer(USEREVENT + 1, DELAY)
+			if mug.hit(coffee5):
+				coffee5.move()
+				hits += 1
+				time.set_timer(USEREVENT + 1, DELAY)
+			if mug.hit(kitten):
+				kitten.move()
+				hits -= 1
+				time.set_timer(USEREVENT + 1, DELAY)
 		elif event.type == USEREVENT + 1:
-			coffee.move()
+			coffee1.move()
+			coffee2.move()
+			coffee3.move()
+			coffee4.move()
+			coffee5.move()
+			kitten.move()
 
 	# copy background image to screen
 	screen.blit(background_image, background_position)
-	text = f.render("Score = " + str(hits), False, (0,0,0))
-	screen.blit(text, (5, 0))
+	score_text = f1.render("Score = " + str(hits), False, (0,0,0))
+	screen.blit(score_text, (5, 0))
 
+	total_sec = start_time - (frame_count // frame_rate)
+	if total_sec < 0:
+		total_sec = 0
+		game_over_text = f2.render("GAME OVER", False, (255,0,0))
+		text_rect = game_over_text.get_rect(center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+		screen.blit(game_over_text, text_rect)
+
+	minutes = total_sec // 60
+	seconds = total_sec % 60
+	countdown = "Time remaining: {0:02}:{1:02}".format(minutes, seconds)
+	countdown_text = f1.render(countdown, True, (0,0,0))
+	screen.blit(countdown_text, (300,0))
+	frame_count += 1
+	clock.tick(frame_rate)
+	
+	coffee_sprites.update()
+	coffee_sprites.draw(screen)
 	sprites.update()
 	sprites.draw(screen)
 	display.update()
 
-#pygame.mixer.music.stop()
+pygame.mixer.music.stop()
 pygame.quit()
 quit() # exits python
